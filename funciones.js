83fe0a6ccdaa1913e58dbe72e6e3fe54a3548252
LaraPuppo <128708137+LaraPuppo@users.noreply.github.com>
2023-09-03 23:07:08 -0300

    funciones.js

--- funciones.js
+++ funciones.js
@@ -1,80 +1,95 @@
-function iniciarFrutas() {
-  for (let i = 0; i < 10; i++) {
-    ellipseX[i] = random(200, 700);
-    ellipseY[i] = 800;
-    ellipsemovX[i] = random(-3, 3);
-    ellipsemovY[i] = random(-20, -15);
-    ellipsegravedad[i] = 0.4;
-  }
+let rectX = [], rectY = [], rectmovX = [], rectmovY = [], rectgravedad = [];
+let rect2X = [], rect2Y = [], rect2movX = [], rect2movY = [], rect2gravedad = [];
+let estado, boton, cantF, cantB, seconds, segundos, difsegundos, tiempoInicio;
+let distancia1, distancia2;
+
+function setup() {
+  createCanvas(900, 600);
+  iniciarFrutas();
+  iniciarBombas();
+  estado = 0;
+  cantF = 1;
+  cantB = 1;
+  boton = true;
+  tiempoInicio = millis();
 }
 
-function moverFrutas () {
-  for (let i = 0; i < 10; i++) {
-    ellipseX[i] += ellipsemovX[i];
-    ellipseY[i] += ellipsemovY[i];
-    ellipsemovY[i] += ellipsegravedad[i];
-    if (ellipseY[i] > height) {
-      ellipseX[i] = random(300, 600);
-      ellipseY[i] = height;
-      ellipsemovX[i] = random(-4, 4);
-      ellipsemovY[i] = random(-20, -15);
-    }
-  }
-}
-
-function dibujarFrutas (numero) {
-  for (let i = 0; i < cantF; i++) {
-    push ();
-    fill (255);
-    ellipse(ellipseX[i], ellipseY[i], 70, 70);
-    pop();
-  }
-}
-
-function iniciarBombas() {
-  for (let i = 0; i < 5; i++) {
-    ellipse2X[i] = random(200, 700); 
-    ellipse2Y[i] = 800;
-    ellipse2movX[i] = random(-3, 3); 
-    ellipse2movY[i] = random(-20, -15);
-    ellipse2gravedad[i] = 0.4;
-  }
-}
-
-function moverBombas () {
-  for (let i = 0; i < 5; i++) {
-    ellipse2X[i] += ellipse2movX[i];
-    ellipse2Y[i] += ellipse2movY[i];
-    ellipse2movY[i] += ellipse2gravedad[i];
-    if (ellipse2Y[i] > height) {
-      ellipse2X[i] = random(300, 600);
-      ellipse2Y[i] = height;
-      ellipse2movX[i] = random(-4, 4);
-      ellipse2movY[i] = random(-20, -15);
-    }
-  }
-}
-
-function dibujarBombas (numero) {
-  for (let i = 0; i < cantB; i++) {
+function draw() {
+  console.log (estado);
+  background(220);
+  text("x:"+ mouseX + ",y:"+ mouseY, mouseX, mouseY);
+  if (estado === 0) {
+    boton = ( mouseX > 300 ) && ( mouseX < 300+300 ) && ( mouseY > 500) && ( mouseY < 500+50);
     push();
-  //rectMode (CENTER);
-    fill(0);
-  //rect (rectX[i], rectY [i], 80,80);
-    ellipse(ellipse2X[i], ellipse2Y[i], 80,80); 
+    textSize (40);
+    textAlign (CENTER, CENTER);
+    textStyle (BOLD);
+    text ("Fruit Ninja", width/2, 100);
+    textSize(20);
+    text ("REGLAS:", width/2, 150);
+    textStyle (NORMAL);
+    text ("1- Corta Frutas:", width/2, 200);
+    text ("Tu objetivo principal es cortar la mayor cantidad de frutas posible en el tiempo asignado", width/2, 230);
+    text ("2- Tres Vidas:", width/2, 280);
+    text ("Comienzas con tres vidas", width/2, 310);
+    text ("Pierdes una vida si dejas caer una fruta sin cortarla antes de que desaparezca de la pantalla", width/2, 330);
+    text ("3- Evita las Bombas:", width/2, 380);
+    text ("Debes evitar a toda costa cortar las bombas que aparecen junto a las frutas", width/2, 410);
+    text ("Si cortas una bomba, el juego terminará inmediatamente", width/2, 430);
+    rect (300, 500, 300, 50);
     pop();
   }
+  if (estado === 1) {
+    background (200, 100, 255);
+    text ("PERDISTE", width/2, height/2);
+  }
+  if (estado ===2) {
+    background (100, 255, 255);
+    text ("GANASTE", width/2, height/2);
+  }
+  if (estado === 3) {
+    timer();
+    moverFrutas();
+    dibujarFrutas(cantF);
+    let tiempoActual = millis();
+    let tiempoTranscurrido = tiempoActual - tiempoInicio;
+    if ( tiempoTranscurrido >= 4000 ) {
+      estado = 4;
+      tiempoInicio = millis();
+    }
+  }
+  if (estado === 4) {
+    timer();
+    cantF = 2;
+    moverFrutas();
+    dibujarFrutas(cantF);
+    moverBombas();
+    dibujarBombas(cantB);
+  }
 }
+function mousePressed () {
+  if (boton) {
+    estado = 3;
+  }
 
-function timer() {
-  push();
-  textSize(40);
-  fill(0);
-  let seconds = millis() / 1000;
-  let sec = floor(seconds) % 60;
-  let min = floor(seconds / 60);
-  let formattedMin = nf(min, 2);
-  let formattedSec = nf(sec, 2);
-  text(formattedMin + ":" + formattedSec, 10, 50);
-  pop();
+  //if (estado === 4) {
+    for (let i = 0; i < cantB; i++) {
+     // distancia2 = dist (mouseX, mouseY, rect2X[i], rect2Y[i]);
+   //   if (distancia2 <= 40) {
+      if (mouseX + 100 > rect2x[i] && mouseX < rect2x[i] + 100 && mouseY + 100 > rect2y[i] && mouseY < rect2y[i] + 100){
+        estado = 1;
+      }
+    //}
+  }
+  
+  for (let i = 0; i < cantF; i++) {
+   // distancia1 = dist (mouseX, mouseY, rectX[i], rectY[i]);
+   // if (distancia1 <= 35) {
+     if (mouseX + 100 > rectx[i] && mouseX < rectx[i] + 100 && mouseY + 100 > recty[i] && mouseY < recty[i] + 100){
+      rectX[i] = -100;
+      rectY[i] = -100;
+      rectmovX[i] = 0;
+      rectmovY[i] = 0;
+    }
+  }
 }
