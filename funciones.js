6a35a53a2b198ca1cffeac7402f28d649b05a171
LaraPuppo <128708137+LaraPuppo@users.noreply.github.com>
2023-09-04 01:15:36 -0300

    funciones.js

--- funciones.js
+++ funciones.js
@@ -4,7 +4,7 @@ function iniciarFrutas() {
     rectY[i] = 800;
     rectmovX[i] = random(-3, 3);
     rectmovY[i] = random(-20, -15);
-    rectgravedad[i] = 0.4;
+    rectgravedad[i] = 0.3;
   }
 }
 
@@ -24,42 +24,46 @@ function moverFrutas () {
 
 function dibujarFrutas (numero) {
   for (let i = 0; i < cantF; i++) {
-    push ();
-    fill (255);
-    rect(rectX[i], rectY[i], 100, 100);
+    push();
+    noFill();
+    noStroke();
+    rect(rectX[i], rectY[i], 150, 150);
+    image(frutas[i], rectX[i], rectY[i], 150, 150);
     pop();
   }
 }
 
 function iniciarBombas() {
-  for (let i = 0; i < 5; i++) {
-    rect2X[i] = random(200, 700); 
-    rect2Y[i] = 800;
-    rect2movX[i] = random(-3, 3); 
-    rect2movY[i] = random(-20, -15);
-    rect2gravedad[i] = 0.4;
+  for (let j = 0; j < 5; j++) {
+    rect2X[j] = random(200, 700);
+    rect2Y[j] = 800;
+    rect2movX[j] = random(-3, 3);
+    rect2movY[j] = random(-20, -15);
+    rect2gravedad[j] = 0.4;
   }
 }
 
 function moverBombas () {
-  for (let i = 0; i < 5; i++) {
-    rect2X[i] += rect2movX[i];
-    rect2Y[i] += rect2movY[i];
-    rect2movY[i] += rect2gravedad[i];
-    if (rect2Y[i] > height) {
-      rect2X[i] = random(300, 600);
-      rect2Y[i] = height;
-      rect2movX[i] = random(-4, 4);
-      rect2movY[i] = random(-20, -15);
+  for (let j = 0; j < 5; j++) {
+    rect2X[j] += rect2movX[j];
+    rect2Y[j] += rect2movY[j];
+    rect2movY[j] += rect2gravedad[j];
+    if (rect2Y[j] > height) {
+      rect2X[j] = random(300, 600);
+      rect2Y[j] = height;
+      rect2movX[j] = random(-4, 4);
+      rect2movY[j] = random(-20, -15);
     }
   }
 }
 
 function dibujarBombas (numero) {
-  for (let i = 0; i < cantB; i++) {
+  for (let j = 0; j < cantB; j++) {
     push();
-    fill(0);
-    rect(rect2X[i], rect2Y[i], 100,100); 
+    noFill();
+    noStroke();
+    rect(rect2X[j], rect2Y[j], 150, 150);
+    image(bomba, rect2X[j], rect2Y[j], 150, 150);
     pop();
   }
 }
@@ -75,4 +79,14 @@ function timer() {
   let formattedSec = nf(sec, 2);
   text(formattedMin + ":" + formattedSec, 10, 50);
   pop();
+}
+
+function cargaArchivos () {
+  for (let i = 0; i < 9; i++) {
+    frutas[i] = loadImage ('assets/fruta'+ i + '.png');
+  }
+  fondo = loadImage ('assets/fondo.jpg');
+  bomba = loadImage ('assets/bomba.png');
+  X = loadImage ('assets/X.png');
+  miFuente = loadFont ('assets/go3v2.ttf');
 }
